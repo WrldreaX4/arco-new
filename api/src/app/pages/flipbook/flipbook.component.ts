727e82dd7970213cad8d5aa7014c09924a38a385
Gwynette2004 <157551832+Gwynette2004@users.noreply.github.com>
2024-05-10 23:04:19 +0800

    Flipbook

--- api/src/app/pages/flipbook/flipbook.component.ts
+++ api/src/app/pages/flipbook/flipbook.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { SummaryComponent } from '../summary/summary.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { ReportComponent } from '../report/report.component';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-flipbook',
   standalone: true,
-  imports: [],
+  imports: [SummaryComponent, NavbarComponent, RouterLink, RouterOutlet, RouterModule, ReportComponent, ProfileComponent],
   templateUrl: './flipbook.component.html',
   styleUrl: './flipbook.component.css'
 })
