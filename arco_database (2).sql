3ce4e3c3d741996d257557e876923a0a408bf273
frederick1018 <frederick.sanford89@gmail.com>
2024-05-21 23:06:25 +0800

    arco db

---
+++ arco_database (2).sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2024 at 05:04 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arco_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `annualreports`
+--
+-- Error reading structure for table arco_database.annualreports: #1932 - Table &#039;arco_database.annualreports&#039; doesn&#039;t exist in engine
+-- Error reading data for table arco_database.annualreports: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `arco_database`.`annualreports`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `collage`
+--
+-- Error reading structure for table arco_database.collage: #1932 - Table &#039;arco_database.collage&#039; doesn&#039;t exist in engine
+-- Error reading data for table arco_database.collage: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `arco_database`.`collage`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventexpenses`
+--
+-- Error reading structure for table arco_database.eventexpenses: #1932 - Table &#039;arco_database.eventexpenses&#039; doesn&#039;t exist in engine
+-- Error reading data for table arco_database.eventexpenses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `arco_database`.`eventexpenses`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventreports`
+--
+-- Error reading structure for table arco_database.eventreports: #1932 - Table &#039;arco_database.eventreports&#039; doesn&#039;t exist in engine
+-- Error reading data for table arco_database.eventreports: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `arco_database`.`eventreports`&#039; at line 1
+<div class="alert alert-danger" role="alert"><h1>Error</h1><p><strong>SQL query:</strong>  <a href="#" class="copyQueryBtn" data-text="SET SQL_QUOTE_SHOW_CREATE = 1">Copy</a>
+<a href="index.php?route=/database/sql&sql_query=SET+SQL_QUOTE_SHOW_CREATE+%3D+1&show_query=1&db=arco_database"><span class="text-nowrap"><img src="themes/dot.gif" title="Edit" alt="Edit" class="icon ic_b_edit">&nbsp;Edit</span></a>    </p>
+<p>
+<code class="sql"><pre>
+SET SQL_QUOTE_SHOW_CREATE = 1
+</pre></code>
+</p>
+<p>
+    <strong>MySQL said: </strong><a href="./url.php?url=https%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F8.0%2Fen%2Fserver-error-reference.html" target="mysql_doc"><img src="themes/dot.gif" title="Documentation" alt="Documentation" class="icon ic_b_help"></a>
+</p>
+<code>#2006 - MySQL server has gone away</code><br></div>
